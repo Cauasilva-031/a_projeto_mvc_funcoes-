1b353de859f5ce08da5e0d9efb86698957d33d81
Cauã Silva <deepplay50@gmail.com>
2026-07-29 21:45:59 -0300

    adicionar paginas

---
+++ assets/js/produto.js
@@ -0,0 +1,41 @@
+//Selecionar o formulário e a div de mensagens
+const form = document.getElementById("formProduto");
+const mensagem = document.getElementById("mensagem");
+
+//executa quando o formulario é enviado
+form.addEventListener("submit", async function(evento){
+
+    //impede o recarregamneto da pagina
+    evento.preventDefault();
+
+    //capturar dados do formulario
+    
+    const dados = new FormData(form);
+
+    //mostra NO CONSOLE os dados do form em tabela
+    //console.table(Object.fromEntries(dados.entries()));
+    //console.log("simples");
+
+    //exibe umma mensagem enquanto os dados são enviados
+
+    mensagem.className = "alert alert-info mt-3";
+    mensagem.textContent = "Enviando dados...";
+
+    try {
+        //Envia os dados para o controller
+
+        const resposta = await fetch("controllers/ProdutoController.php",{
+            method: "post",
+            body: dados
+        });
+
+    } catch (erro) {
+        
+        //Exibe mensagem caso ocorra erro
+        mensagem.className = "alert alert-danger mt-3";
+        mensagem.textContent = "Erro ao enviar os dados";
+
+        console.log(erro);
+
+    }
+});
---
+++ controllers/ProdutoController.php
@@ -0,0 +1,72 @@
+<?php
+
+
+//A resposta será enviada em formao json
+header("Content-Type: application/json; charset=utf-8");
+
+//Verifica se a requisição é do tipo POST
+if($_SERVER["REQUEST_METHOD"]!== "POST"){
+    http_response_code(405); //405 -  metodo não permitido
+
+    echo json_encode([
+        "sucesso" => false,
+        "mensagem" => "Metodo não permitido, esperava GET"
+
+    ]);
+
+    exit;
+
+}
+
+//Recebe os dados enviados pelo formulário
+$nome = trim($_POST["nome"]);
+$categoria = trim($_POST["categoria"]);
+$preco = trim($_POST["preco"]);
+$quantidade = trim($_POST["quantidade"]);
+
+//Valida os campos obrigatórios
+
+if($nome === "" || $categoria === "" || $preco === "" || $quantidade === ""){
+    http_response_code(400);
+    echo json_encode([
+        "sucesso" => false,
+        "mensagem" => "Preencha todos os campos"
+    ]);
+
+exit;
+
+}
+
+ // ---------->>> TODO: Aqui seria o banco de dados 
+
+
+
+//Retornar sucesso
+http_response_code(200);
+
+echo json_encode([
+    "sucesso" => true,
+    "mensagem" => "Produto cadastrado com sucesso!",
+    "produto" => [
+        "nome" => $nome,
+        "categoria" => $categoria,
+        "preco" => $preco,
+        "quantidade" => $quantidade,
+
+
+
+    ]
+])
+
+
+
+
+
+
+
+
+
+
+
+
+?>
