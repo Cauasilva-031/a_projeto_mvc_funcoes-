8f2f7e2316c6523f7927b4386a3fa8fb175ffc96
Cauã Silva <deepplay50@gmail.com>
2026-07-31 20:18:46 -0300

    Atualizções

--- routes.php
+++ routes.php
@@ -1,25 +1,21 @@
 <?php
-//definir url do projeto
-//http://localhost/projetos-caua/mvc/a_projeto_mvc_funcoes/index.php?page=produto
+//definir url do  projeto
+//http://localhost/projetos-michelle/mvc/a_projeto_mvc_funcoes/index.php?page=produtos
 
-//definir paginas válidas no projeto
-
-
-
+//definir páginas válidas no projeto
 $paginasValidas = [
     "produtos" => __DIR__ . "/views/produto.php",
     "clientes" => __DIR__ . "/views/cliente.php",
     "funcionarios" => __DIR__ . "/views/funcionario.php",
-
 ];
 
-// Capturar a página informada na url
-$page = $_GET["page"] ?? "produtos";
+// Capturar a página informada na url 
+$page = $_GET["page"] ?? "produtos"; //prod. ou cli. ou func.
 
-// Verifica se a página existe
-if(array_key_exists($page, $paginasValidas)){
+//Verificar se a página existe
+if (array_key_exists($page, $paginasValidas)) {
     require $paginasValidas[$page];
-}else{
+} else {
     http_response_code(404);
     require __DIR__ . "/views/404.php";
-} 
+}
